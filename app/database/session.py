a376db7c6e7043cfc8667b687e97a211baf97a5b
ventura-christian <cventura64@outlook.com>
2026-06-27 14:00:19 -0700

    add session.py

---
+++ app/database/session.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "")
+
+# This tells me where the database is located
+engine = create_engine(DATABASE_URL)
+
+# This allows a connection to be opened
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# This is the class the models will inherit from
+Base = declarative_base()
